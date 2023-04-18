58f3c09f1c36c3c6fdad2d9851d2ad116b6584c4
EspressoIntenso <maxbracken1@gmail.com>
2023-04-18 17:33:53 +1200

    Added functionality from original discord bot

--- app/src/main/java/com/example/dishbishv2/MainActivity.java
+++ app/src/main/java/com/example/dishbishv2/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.dishbishv2;
 
+import static java.time.temporal.ChronoUnit.DAYS;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.time.Duration;
+import java.time.LocalDate;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        String[] order = {"Max", "Ellisha", "Tupuna", "Nathan", "Rory",
+                "Ellisha", "Rory",
+                "Max", "Ellisha", "Tupuna", "Nathan", "Rory",
+                "Max", "Nathan",
+                "Max", "Ellisha", "Tupuna", "Nathan", "Rory",
+                "No one", "Tupuna"};
+
         setContentView(R.layout.activity_main);
+        TextView total = (TextView) findViewById(R.id.who);
+
+        int index = 0;
+        LocalDate startDate = LocalDate.of(2022, 11, 14);
+        LocalDate currDate = LocalDate.now();
+        Duration differenceInTime = Duration.ofDays(DAYS.between(startDate, currDate));
+        long differenceInDays = differenceInTime.toDays();
+        index = (int)differenceInDays  % 21;
+        String output = order[index];
+        System.out.println(output);
+        total.setText(output);
     }
 }
